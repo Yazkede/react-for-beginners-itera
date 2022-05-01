1e82b2e8095602419ce8249eb2aeacfad867d101
Yazkede <missclick79@gmail.com>
2022-05-01 14:54:13 +0300

    dz1

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h2>username: Oleksandr</h2>
+      <h3>age: 42</h3>
+      <h4>email: mypost@gmail.com</h4>
+      <h4>phone: 322223322223</h4>
+    </div>
+  );
+}
+
+export default App;
